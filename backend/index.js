31c7013dd1963844b236479affc00e905262984b
Ashnab-khan <tyesionkhan2001.com>
2025-11-13 17:31:12 +0530

    fs is not defined at backend we have to import

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv";
 
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
